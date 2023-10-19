59bccdd9510052ce336f505300964f13da558526
Josué Nunes <josueemcontato@gmail.com>
2023-10-19 11:53:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# password-generator
+ Um gerador de senhas desenvolvido com React. 
---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Input from "./components/input";
+
+function App() {
+  const [password, setPassword] = useState("");
+  const [customdSize, setCustomSize] = useState(12);
+  const [showInput, setShowInput] = useState(false);
+  const [copyText, setCopyText] = useState("Copiar");
+  const passwordSize = showInput ? customdSize : 8;
+  function generate() {
+    const characters =
+      "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
+    let newPassword = "";
+    for (let i = 0; i < passwordSize; i++) {
+      const position = Math.floor(Math.random() * characters.length);
+      newPassword += characters[position];
+    }
+    setPassword(newPassword);
+    setCopyText("Copiar");
+  }
+
+  function copyToClipboard() {
+    window.navigator.clipboard.writeText(password);
+    setCopyText("Copiado");
+  }
+  return (
+    <div>
+      <h1>Gerador de senhas</h1>
+      <div>
+        <label htmlFor="showInput">Customizar o tamanho</label>
+        <input
+          type="checkbox"
+          id="showInput"
+          value={showInput}
+          onChange={() => setShowInput((currentState) => !currentState)}
+        />
+      </div>
+      {showInput && (
+        <div>
+          <label htmlFor="passwordSize">Tamanho:</label>
+          <Input passwordSize={customdSize} setPasswordSize={setCustomSize} />
+        </div>
+      )}
+
+      <button onClick={generate}>
+        Gerar senha de {passwordSize} caracteres
+      </button>
+      <button onClick={copyToClipboard}>{copyText}</button>
+      <div>{password}</div>
+    </div>
+  );
+}
+
+export default App;
